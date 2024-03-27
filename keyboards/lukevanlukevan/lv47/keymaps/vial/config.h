5d2e6bfce1969b0bf90192e9daa64f3395a46bc2
Luke Van <lukevrensburg@gmail.com>
2024-03-27 20:13:26 +0200

    Implement Vandura

---
+++ keyboards/lukevanlukevan/lv47/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Vial
+
+#define VIAL_KEYBOARD_UID {0x91, 0xB4, 0x78, 0x4C, 0xDD, 0x97, 0xD2, 0x75}
+#define VIAL_UNLOCK_COMBO_ROWS { 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0 }
---
+++ keyboards/lukevanlukevan/vandura/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Vial
+
+#define VIAL_KEYBOARD_UID {0x91, 0xB4, 0x78, 0x4C, 0xDD, 0x97, 0xD2, 0x75}
+#define VIAL_UNLOCK_COMBO_ROWS { 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
